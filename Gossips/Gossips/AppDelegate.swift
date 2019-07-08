d31c4b49ae1dab86c8037cd2715d7ad2b5d35381
Mahmoud Elkassry <mahmoud.elkassry@robustastudio.com>
2019-07-08 12:52:59 +0200

    added auth VC

--- Gossips/Gossips/AppDelegate.swift
+++ Gossips/Gossips/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if UserDefaults.standard.string(forKey: "phoneNumber") != nil{
+            window?.rootViewController = FriendsViewController()
+        }
+        application.statusBarStyle = .lightContent
         FirebaseApp.configure()
         IQKeyboardManager.shared.enable = true
         return true
